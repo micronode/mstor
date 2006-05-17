f11085d1a14980dfe5f068b5918259ea1bd48d0f
fortuna <fortuna@users.sourceforge.net>
2006-05-17 09:37:57 +0000

    Added test suite for all tests

---
+++ test/net/fortuna/mstor/AllTests.java
@@ -0,0 +1,69 @@
+/*
+ * $Id$
+ *
+ * Created on 14/05/2006
+ *
+ * Copyright (c) 2005, Ben Fortuna
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  o Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of Ben Fortuna nor the names of any other contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.fortuna.mstor;
+
+import net.fortuna.mstor.data.MboxEncoderTest;
+import net.fortuna.mstor.data.MboxFileTest;
+import net.fortuna.mstor.search.TagsTermTest;
+import junit.framework.TestSuite;
+
+/**
+ * A suite of all unit tests.
+ * @author Ben Fortuna
+ */
+public class AllTests extends TestSuite {
+
+    /**
+     * @return a suit of unit tests.
+     */
+    public static TestSuite suite() {
+        TestSuite suite = new TestSuite();
+        
+        // javamail..
+        suite.addTestSuite(MStorStoreTest.class);
+        
+        // mbox..
+        suite.addTestSuite(MboxEncoderTest.class);
+        suite.addTestSuite(MboxFileTest.class);
+        
+        // tags..
+        suite.addTestSuite(TagsTest.class);
+        suite.addTestSuite(TagTest.class);
+        suite.addTestSuite(TagsTermTest.class);
+        
+        return suite;
+    }
+}
