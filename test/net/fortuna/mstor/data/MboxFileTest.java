4b212a02f062a4266540656e3e83e3926b6479aa
fortuna <fortuna@users.sourceforge.net>
2006-06-10 13:29:54 +0000

    Added suite to test all mbox capability hints

--- test/net/fortuna/mstor/data/MboxFileTest.java
+++ test/net/fortuna/mstor/data/MboxFileTest.java
@@ -10,7 +10,10 @@ package net.fortuna.mstor.data;
 import java.io.File;
 import java.io.IOException;
 
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import net.fortuna.mstor.util.CapabilityHints;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -25,15 +28,40 @@ public class MboxFileTest extends TestCase {
 
     private MboxFile mbox;
 
+    /**
+     * @param method
+     */
+    public MboxFileTest(String method) {
+        super(method);
+    }
+    
+    /**
+     * @param bufferStrategy
+     * @param cacheStrategy
+     */
+    public MboxFileTest(String method, String bufferStrategy, String cacheStrategy) {
+        super(method);
+        CapabilityHints.setHint(CapabilityHints.KEY_MBOX_BUFFER_STRATEGY,
+                bufferStrategy);
+        CapabilityHints.setHint(CapabilityHints.KEY_MBOX_CACHE_BUFFERS,
+                cacheStrategy);
+    }
+    
     /*
      * @see TestCase#setUp()
      */
     protected final void setUp() throws Exception {
         super.setUp();
-
         File f = new File("etc/samples/MboxFile/Inbox");
-
         mbox = new MboxFile(f, MboxFile.READ_WRITE);
+    }
+    
+    /* (non-Javadoc)
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        mbox.close();
     }
 
     /**
@@ -43,8 +71,6 @@ public class MboxFileTest extends TestCase {
         assertTrue(mbox.getMessageCount() >= 0);
 
         log.info("Message count: " + mbox.getMessageCount());
-
-        mbox.close();
     }
 
     /**
@@ -60,7 +86,6 @@ public class MboxFileTest extends TestCase {
             log.info("Message [" + i + "]\n=================\n"
                     + new String(buffer));
         }
-        mbox.close();
     }
 
     /**
@@ -70,7 +95,6 @@ public class MboxFileTest extends TestCase {
      */
     public final void testPurge() throws IOException {
         mbox.purge(new int[] {1});
-        mbox.close();
     }
 
     /*
@@ -93,4 +117,23 @@ public class MboxFileTest extends TestCase {
      * System.currentTimeMillis()); // remove any existing temporary files.. if
      * (tempFile.exists()) { tempFile.delete(); } f.renameTo(tempFile); }
      */
+    
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        suite.addTest(new MboxFileTest("testGetMessage",
+                CapabilityHints.VALUE_MBOX_BUFFER_STRATEGY_DEFAULT,
+                CapabilityHints.VALUE_MBOX_CACHE_BUFFERS_DISABLED));
+        suite.addTest(new MboxFileTest("testGetMessage",
+                CapabilityHints.VALUE_MBOX_BUFFER_STRATEGY_DIRECT,
+                CapabilityHints.VALUE_MBOX_CACHE_BUFFERS_DISABLED));
+        suite.addTest(new MboxFileTest("testGetMessage",
+                CapabilityHints.VALUE_MBOX_BUFFER_STRATEGY_MAPPED,
+                CapabilityHints.VALUE_MBOX_CACHE_BUFFERS_DISABLED));
+        suite.addTest(new MboxFileTest("testGetMessage",
+                CapabilityHints.VALUE_MBOX_BUFFER_STRATEGY_DEFAULT,
+                CapabilityHints.VALUE_MBOX_CACHE_BUFFERS_ENABLED));
+        suite.addTest(new MboxFileTest("testGetMessageCount"));
+        suite.addTest(new MboxFileTest("testPurge"));
+        return suite;
+    }
 }
