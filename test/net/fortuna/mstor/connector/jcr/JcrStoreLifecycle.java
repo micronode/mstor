13a866ca2e523ddc79438c9188302473049d8b49
fortuna <fortuna@users.sourceforge.net>
2008-05-07 10:51:47 +0000

    Added lifecycle skeleton

---
+++ test/net/fortuna/mstor/connector/jcr/JcrStoreLifecycle.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package net.fortuna.mstor.connector.jcr;
+
+import javax.mail.Store;
+
+import net.fortuna.mstor.StoreLifecycle;
+
+/**
+ * @author fortuna
+ *
+ */
+public class JcrStoreLifecycle implements StoreLifecycle {
+
+	/* (non-Javadoc)
+	 * @see net.fortuna.mstor.StoreLifecycle#getStore()
+	 */
+	public Store getStore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.fortuna.mstor.StoreLifecycle#shutdown()
+	 */
+	public void shutdown() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see net.fortuna.mstor.StoreLifecycle#startup()
+	 */
+	public void startup() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
