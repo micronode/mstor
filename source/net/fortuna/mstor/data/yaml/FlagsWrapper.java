ec0f5edb2b4e8d6ccfdbf64988fcdf8aeb9aa1fe
fortuna <fortuna@users.sourceforge.net>
2008-05-10 07:24:37 +0000

    Added custom handlers for flags

---
+++ source/net/fortuna/mstor/data/yaml/FlagsWrapper.java
@@ -0,0 +1,82 @@
+/*
+ * $Id$
+ *
+ * Created on 10/05/2008
+ *
+ * Copyright (c) 2008, Ben Fortuna
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  o Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of Ben Fortuna nor the names of any other contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.fortuna.mstor.data.yaml;
+
+import javax.mail.Flags;
+import javax.mail.Flags.Flag;
+
+import org.ho.yaml.wrapper.DelayedCreationBeanWrapper;
+
+/**
+ * @author Ben
+ *
+ */
+public class FlagsWrapper extends DelayedCreationBeanWrapper {
+
+    /**
+     * @param type
+     */
+    public FlagsWrapper(Class type) {
+        super(type);
+    }
+
+    /* (non-Javadoc)
+     * @see org.ho.yaml.wrapper.DelayedCreationBeanWrapper#getPropertyNames()
+     */
+    @Override
+    public String[] getPropertyNames() {
+        return new String[] {"systemFlags", "userFlags"};
+    }
+
+    @Override
+    protected Object createObject() {
+        Flags flags = (Flags) createPrototype();
+        Flag[] systemFlags = (Flag[]) values.get("systemFlags");
+        for (int i = 0; i < systemFlags.length; i++) {
+            flags.add(systemFlags[i]);
+        }
+        String[] userFlags = (String[]) values.get("userFlags");
+        for (int i = 0; i < userFlags.length; i++) {
+            flags.add(userFlags[i]);
+        }
+        return flags;
+    }
+    
+    @Override
+    public Object createPrototype() {
+        return new Flags();
+    }
+}
