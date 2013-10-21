ed77587b2b08d215ab3c477a876bd41125f4c5f6
Ben Fortuna <benfortuna@gmail.com>
2013-10-21 14:41:05 +1100

    Updated to junit4 test suite

--- src/test/java/net/fortuna/mstor/connector/mbox/MboxTagIntegrationTest.java
+++ src/test/java/net/fortuna/mstor/connector/mbox/MboxTagIntegrationTest.java
@@ -56,7 +56,7 @@ import org.apache.commons.io.filefilter.NotFileFilter;
  *
  */
 @RunWith(AllTests.class)
-public class MboxTagIntegrationTest extends TestSuite {
+public class MboxTagIntegrationTest {
 
     /**
      * @return
